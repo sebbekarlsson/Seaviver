8da98875998f04ec9d68a4ccfb8a95e0c770fb84
= <sebbekarlsson97@gmail.com>
2015-09-24 21:45:56 +0200

    transparent

--- src/seaviver/main/Seaviver.java
+++ src/seaviver/main/Seaviver.java
@@ -219,6 +219,8 @@ public class Seaviver {
      	GL11.glLoadIdentity();
      	GL11.glEnable(GL11.GL_TEXTURE_2D);
      	GL11.glEnable(GL11.GL_DEPTH_TEST);
+     	GL11.glEnable(GL11.GL_BLEND);
+     	GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
 	}
 	
 	/**
